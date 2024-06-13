b149954b2cd1679bf37c88e884f5ebfb1444ecf4
l1chow <leminhchau172003@gmail.com>
2024-06-13 11:39:22 +0700

    update 5.2

--- 2_nhap_xuat_ky_tu/2d.asm
+++ 2_nhap_xuat_ky_tu/2d.asm
@@ -1,30 +1,32 @@
 DSEG SEGMENT
-    tbao DB 'Hay go vao 30 ky tu: $' 
-    buffer db 30 dup(?)
+    max DB 200
+    len DB 0      
+    chuoi DB 30 dup('$') ;giong khai bao mang
+    tbao DB 'Hay go vao 1 chuoi: $'
 DSEG ENDS
-
 CSEG SEGMENT
-    ASSUME CS:CSEG, DS:DSEG
-start:
-    mov ax, DSEG
+    ASSUME CS: CSEG, DS: DSEG
+start: mov ax, DSEG
     mov ds, ax
-
-    mov ah, 09h        
+    mov ah, 09h 
     lea dx, tbao
-    int 21h
-
-    mov cx, 30        
-    lea si, buffer    
-
-input_loop:
-    mov ah, 01h        
-    int 21h            
-    mov [si], al       
-    inc si            
-    loop input_loop    
-
-    mov ah, 4Ch       
-    int 21h
+    int 21h      
+    
+    mov ah, 0Ah
+    lea dx, max 
+    int 21h 
+      
+    ;mov ah,9 
+    ;lea dx,chuoi+2
+    ;int 21h     
+    
+    mov ah, 4Ch
+    int 21h   
 
 CSEG ENDS
-END start
+    END start
+
+;sua chtr de nhap nhieu hon 30 ky tu
+;sua max len 200 thi ko can "chuoi DB 30,dup('$)"
+; sua chuoi len 200 thi "chuoi DB 30,dup('$)" 
+;va thay lea dx,chuoi o hang 16
