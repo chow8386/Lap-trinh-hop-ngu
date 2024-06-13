bb64810b4e7aada994e485c6bb0fee1a84245e86
l1chow <leminhchau172003@gmail.com>
2024-06-13 11:38:04 +0700

    upload 5.2

--- 2_nhap_xuat_ky_tu/5.2.asm
+++ 2_nhap_xuat_ky_tu/5.2.asm
@@ -1,24 +1,30 @@
 DSEG SEGMENT
-    max DB 30
-    len DB 0  ;luu do dai ky tu da nhap
-    chuoi DB 30 dup(?)
-    tbao DB 'Hay go vao 1 chuoi: $'
+    tbao DB 'Hay go vao 30 ky tu: $' 
+    buffer db 30 dup(?)
 DSEG ENDS
+
 CSEG SEGMENT
-    ASSUME CS: CSEG, DS: DSEG
-start: mov ax, DSEG
+    ASSUME CS:CSEG, DS:DSEG
+start:
+    mov ax, DSEG
     mov ds, ax
-    mov ah, 09h 
+
+    mov ah, 09h        
     lea dx, tbao
-    int 21h      
-    
-    mov ah, 0Ah 
-    lea dx, MAX 
-    int 21h     
-    
-    mov ah, 4Ch 
     int 21h
+
+    mov cx, 30        
+    lea si, buffer    
+
+input_loop:
+    mov ah, 01h        
+    int 21h            
+    mov [si], al       
+    inc si            
+    loop input_loop    
+
+    mov ah, 4Ch       
+    int 21h
+
 CSEG ENDS
-    END start
-    
-;chuoi nhap vao se luu o bien 'chuoi'
+END start
